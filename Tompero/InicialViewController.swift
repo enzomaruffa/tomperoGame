029e3027255aff25902c4dc38888401add444775
Akira Tsukamoto <akiratsukamoto13@gmail.com>
2019-11-25 09:40:35 -0300

    inicialviewcontroller

--- Tompero/InicialViewController.swift
+++ Tompero/InicialViewController.swift
@@ -1,56 +1,51 @@
 import UIKit
 
 class InicialViewController: UIViewController {
-
+    
     
     @IBOutlet weak var person: UIImageView!
     var location = CGPoint(x: 0, y: 0)
-    
     @IBOutlet weak var join: UIImageView!
-    
     @IBOutlet weak var host: UIImageView!
     
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-            
-            var touch : UITouch! =  touches.first! as UITouch
-            
-        if touch.location(in: self.view) == person.center {
-            location = touch.location(in: self.view)
-            
-            person.center = location
-        }
-            
-            
-    }
-
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-            
-            var touch : UITouch! =  touches.first! as UITouch
-            
-         if touch.location(in: self.view) == person.center {
-                   location = touch.location(in: self.view)
-                   
-                   person.center = location
-               }
-        }
-    
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        var touch : UITouch! =  touches.first! as UITouch
-        if join.frame.intersects(person.frame) {
-            person.center = join.center
-        }
-        else {
-            person.center = view.frame.origin
-        }
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
         
+        let touch : UITouch! =  touches.first! as UITouch
+        
+        if person.frame.contains(touch.location(in: self.view)){
+            location = touch.location(in: self.view)
+            person.center = location
+            
+            
+        }
+        
+        func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
 
-        // Do any additional setup after loading the view.
+            let touch : UITouch! =  touches.first! as UITouch
+
+            if touch.location(in: self.view) == person.center {
+                location = touch.location(in: self.view)
+                person.center = location
+            }
+        }
+        
+        
+        func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+            let touch : UITouch! =  touches.first! as UITouch
+            if join.frame.intersects(person.frame) {
+                person.center = join.center
+            }
+            else {
+                person.center = view.frame.origin
+            }
+        }
+        func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Do any additional setup after loading the view.
+        }
+        
+        
     }
-    
-
 }
