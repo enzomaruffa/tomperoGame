6031acc41910967061df7ec508867dabddd06259
Vinícius Binder <vinicius.binder@gmail.com>
2019-12-07 01:10:09 -0300

    Finishes up list physics.

--- Tompero/View/GameScene/OrderNode.swift
+++ Tompero/View/GameScene/OrderNode.swift
@@ -14,7 +14,7 @@ class OrderNode: SKSpriteNode {
     var order: Order?
     
     override init(texture: SKTexture?, color: UIColor, size: CGSize) {
-        super.init(texture: texture, color: .red, size: CGSize(width: 10.5, height: 36.0))
+        super.init(texture: texture, color: UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 0), size: CGSize(width: 10.5, height: 36.0))
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -29,14 +29,14 @@ class OrderNode: SKSpriteNode {
     
     private func position(ofIngredient index: Int) -> CGPoint {
         switch index {
-        case 0: return CGPoint(x: -30, y: -19)
-        case 1: return CGPoint(x: -10, y: -19)
-        case 2: return CGPoint(x: 10, y: -19)
-        case 3: return CGPoint(x: 30, y: -19)
-        case 4: return CGPoint(x: -30, y: -37)
-        case 5: return CGPoint(x: -10, y: -37)
-        case 6: return CGPoint(x: 10, y: -37)
-        case 7: return CGPoint(x: 30, y: -37)
+        case 0: return CGPoint(x: -120, y: -90)
+        case 1: return CGPoint(x: -40, y: -90)
+        case 2: return CGPoint(x: 40, y: -90)
+        case 3: return CGPoint(x: 120, y: -90)
+        case 4: return CGPoint(x: -120, y: -170)
+        case 5: return CGPoint(x: -40, y: -170)
+        case 6: return CGPoint(x: 40, y: -170)
+        case 7: return CGPoint(x: 120, y: -170)
         default: return CGPoint()
         }
     }
@@ -45,15 +45,15 @@ class OrderNode: SKSpriteNode {
         for (index, ingredient) in order!.ingredients.enumerated() {
             
             let circle = SKSpriteNode(imageNamed: "IngredientIndicator")
+            self.addChild(circle)
             circle.position = position(ofIngredient: index)
             circle.zPosition = 7
-            circle.size = CGSize(width: 17, height: 17)
+            circle.size = CGSize(width: 60, height: 60)
             
             let node = SKSpriteNode(imageNamed: ingredient.texturePrefix + "Raw")
-            node.zPosition = 8
-            node.size = CGSize(width: 13, height: 13)
-            
             circle.addChild(node)
+            node.zPosition = 8
+            node.size = CGSize(width: 25, height: 25)
         }
     }
     
