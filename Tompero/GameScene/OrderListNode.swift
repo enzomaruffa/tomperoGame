fc288cd3d3dda027ec2a0a0edc8a3b588ffa0cc1
Enzo Maruffa <enzomm1999@gmail.com>
2019-12-12 09:52:18 -0300

    Adds bug annotation

--- Tompero/GameScene/OrderListNode.swift
+++ Tompero/GameScene/OrderListNode.swift
@@ -108,11 +108,12 @@ class OrderListNode: SKSpriteNode {
                 self.physicsBody?.applyImpulse(CGVector(dx: 3000, dy: 0))
             },
             .run {
+                // TODO: Possible bug. Close should only happen after the duration ends
                 self.close()
             }
         ]))
     }
-    
+
     func checkAction() {
         if canChangeCurrentState(to: .open) {
             open()
