dbff523781dc17ca23c85a79fcb9e8d8bed38ff2
Vinícius Binder <vinicius.binder@gmail.com>
2019-12-19 18:15:48 -0300

    Added Game Center leaderboard.

--- Tompero/Controller/StatisticsViewController.swift
+++ Tompero/Controller/StatisticsViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import GameKit
 
-class StatisticsViewController: UIViewController, Storyboarded {
+class StatisticsViewController: UIViewController, Storyboarded, GKGameCenterControllerDelegate {
     
     // MARK: - Storyboarded
     static var storyboardName: String = "Statistics"
@@ -18,6 +19,11 @@ class StatisticsViewController: UIViewController, Storyboarded {
     
     // MARK: - Variables
     var statistics: MatchStatistics!
+    
+    // MARK: - Game Center
+    var isGameCenterEnabled: Bool! // check if Game Center enabled
+    var defaultLeaderboard = "" // check default leaderboard ID
+    let LEADERBOARD_ID = "com.score.spacespice"
     
     // MARK: - Outlets
     @IBOutlet weak var deliveredOrdersLabel: UILabel!
@@ -33,9 +39,44 @@ class StatisticsViewController: UIViewController, Storyboarded {
         deliveredOrdersLabel.text = "\(statistics.totalDeliveredOrders) delivered orders!"
         
         pointsLabel.text = "\(statistics.totalPoints) points!"
+        
+        authenticateLocalPlayer()
+        submitScoreToGameCenter()
     }
     
     // MARK: - Methods
+    func authenticateLocalPlayer() {
+        let localPlayer: GKLocalPlayer = GKLocalPlayer.localPlayer()
+        
+        localPlayer.authenticateHandler = { (viewController, error) -> Void in
+            if viewController != nil {
+                // 1. show login if player is not logged in
+                self.present(viewController!, animated: true, completion: nil)
+            } else if localPlayer.isAuthenticated {
+                // 2. player is already authenticated & logged in, load game center
+                self.isGameCenterEnabled = true
+                
+                // get default leaderboard ID
+                localPlayer.loadDefaultLeaderboardIdentifier(completionHandler: { (leaderboardIdentifer, error) in
+                    if error != nil {
+                        print(error)
+                    } else {
+                        self.defaultLeaderboard = leaderboardIdentifer!
+                    }
+                })
+                
+            } else {
+                // 3. game center is not enabled on the users device
+                self.isGameCenterEnabled = false
+                print("Local player could not be authenticated!")
+                print(error)
+            }
+        }
+    }
+    
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true, completion: nil)
+    }
     
     // MARK: - Actions
     @IBAction func menuPressed(_ sender: Any) {
@@ -44,4 +85,23 @@ class StatisticsViewController: UIViewController, Storyboarded {
         coordinator?.popToRoot()
     }
     
+    func submitScoreToGameCenter() {
+        let score = GKScore(leaderboardIdentifier: LEADERBOARD_ID)
+        score.value = statistics.totalPoints
+        GKScore.report([score]) { (error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                print("Best Score submitted to leaderboard!")
+            }
+        }
+    }
+    
+    func openLeaderboard(_ sender: AnyObject) {
+        let vc = GKGameCenterViewController()
+        vc.gameCenterDelegate = self
+        vc.viewState = .leaderboards
+        vc.leaderboardIdentifier = LEADERBOARD_ID
+        present(vc, animated: true, completion: nil)
+    }
 }
