8cd572de61b5868f83b29601bd4ad6986e2f68cd
Vinícius Binder <vinicius.binder@gmail.com>
2020-04-23 18:51:30 -0300

    Revamped Menu
    Removed asset junk.

--- Tompero/Controller/MenuCollectionViewController.swift
+++ Tompero/Controller/MenuCollectionViewController.swift
@@ -2,30 +2,46 @@ import UIKit
 
 class MenuCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, Storyboarded {
     
+    static var storyboardName = "MenuStoryboard"
+    weak var coordinator: MainCoordinator?
+    
     // MARK: - Outlets
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var collectionViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var collectionViewBottomConstraint: NSLayoutConstraint!
     
     // MARK: - Variables
-    let ingredient: [UIImage] = [UIImage(named: "ingredientes 1")!, UIImage(named: "ingredientes 1")!]
-    weak var coordinator: MainCoordinator?
-    
-    // MARK: - Storyboarded
-    static var storyboardName = "MenuStoryboard"
-    
-    // MARK: - ViewLifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    override func viewDidAppear(_ animated: Bool) {
-    }
-    
     var parentVC: WaitingRoomViewController!
+    
+    let numberOfPages = 2
+    var pages: [UIImage] {
+        var pages: [UIImage] = []
+        for index in 1...numberOfPages {
+            pages.append(UIImage(named: "Menu_page\(index)")!)
+        }
+        return pages
+    }
+    
+    // MARK: - Scene Lifecycle
+    
+    override func viewDidLoad() {
+        pageControl.numberOfPages = numberOfPages
+        
+        if traitCollection.verticalSizeClass == .regular {
+            // if iPad
+            _ = collectionViewBottomConstraint.setMultiplier(multiplier: 1)
+        } else {
+            _ = collectionViewBottomConstraint.setMultiplier(multiplier: 0.86)
+        }
+    }
+    
+    // MARK: - Methods
     
     @IBAction func backPressed(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+    
     // MARK: - Collection Methods
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -34,53 +50,33 @@ class MenuCollectionViewController: UIViewController, UICollectionViewDataSource
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return ingredient.count
+        return pages.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "menuCell", for: indexPath) as! MenuCollectionViewCell
-        cell.menuImage.image = ingredient[indexPath.row]
+        cell.menuImage.image = pages[indexPath.row]
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let viewW = self.view.safeAreaLayoutGuide.layoutFrame.size.width
-        let viewH = self.view.safeAreaLayoutGuide.layoutFrame.size.height
+        let width = view.safeAreaLayoutGuide.layoutFrame.size.width
+        let height = view.safeAreaLayoutGuide.layoutFrame.size.height
         
-        let wMult: CGFloat = 1
-        let hMult: CGFloat = 0.7
-        return CGSize(width: viewW * wMult, height: viewH * hMult)
+        let widthMultiplier: CGFloat = collectionViewWidthConstraint.multiplier
+        return CGSize(width: widthMultiplier * width, height: height)
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 1
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
+    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         pageControl.currentPage = Int(self.collectionView.contentOffset.x)/Int(self.collectionView.frame.width)
     }
     
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
