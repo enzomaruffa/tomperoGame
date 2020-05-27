cdfbf94d624c5bcfa9edf018abebf8f62363ccba
Vinícius Binder <vinicius.binder@gmail.com>
2020-05-27 00:15:21 -0300

    Added settings view selector

--- Tompero/Controller/SettingsViewController.swift
+++ Tompero/Controller/SettingsViewController.swift
@@ -10,19 +10,60 @@ import UIKit
 
 class SettingsViewController: UIViewController, Storyboarded {
     
+    // MARK: - Coordinator
     static var storyboardName = "Settings"
     weak var coordinator: MainCoordinator?
-
-    @IBOutlet weak var boxCenterYConstraint: NSLayoutConstraint!
     
+    // MARK: - Outlets
+    @IBOutlet weak var boxCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet var widthConstraints: [NSLayoutConstraint]!
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var images: [UIImageView]!
+    
+    // MARK: - Variables
+    var selectedState: Int!
+    
+    // MARK: - Constants
+    let selectedFont = UIFont(name: "TitilliumWeb-Bold", size: 26)
+    let defaultFont = UIFont(name: "TitilliumWeb-Light", size: 19)
+    let selectedMultiplier: CGFloat = 0.31
+    var defaultMultiplier: CGFloat!
+    
+    // MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         
         _ = boxCenterYConstraint.setMultiplier(multiplier: traitCollection.verticalSizeClass == .regular ? 1 : 1.15)
         
+        defaultMultiplier = (1 - selectedMultiplier) / 3
+        select(0)
     }
-
+    
     @IBAction func backPressed(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
+    
+    func select(_ state: Int) {
+        selectedState = state
+        
+        widthConstraints.enumerated().forEach { index, constraint in
+            _ = constraint.setMultiplier(multiplier: index == state ? selectedMultiplier : defaultMultiplier)
+        }
+        
+        buttons.enumerated().forEach { index, button in
+            button.titleLabel!.font = index == state ? selectedFont : defaultFont
+            
+        }
+        
+        images.enumerated().forEach { index, image in
+            image.image = UIImage(named: index == state ? "Settings_selectionButtonON" : "Settings_selectionButtonOFF")
+        }
+        
+        // change the view too
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        select((sender as! UIButton).tag)
+    }
+    
 }
